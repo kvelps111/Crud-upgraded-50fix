8db3161be87f583fedda6ac7f1127177b56e2cc6
user <test@gmail.com>
2025-11-27 13:45:23 +0200

    added tags in index view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,17 +1,33 @@
 <x-layout>
     <x-slot:title>
         All products
-    </x-slot>
+    </x-slot:title>
 
     <ul class="product-list">
         @foreach ($products as $product)
             <li>
                 <h1>{{ $product->name }}</h1>
                 <p>{{ $product->description }}</p>
+
+                {{-- PRODUCT TAGS --}}
+                @if ($product->tags->count() > 0)
+                    <div class="product-tags">
+                        <strong>Birkas:</strong>
+                        <ul>
+                            @foreach ($product->tags as $tag)
+                                <li class="tag-badge">{{ $tag->name }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @else
+                    <p class="no-tags">No tags</p>
+                @endif
+
                 <div class="product-actions">
-                    <a href="{{ route('products.show', [$product]) }}" class="btn btn-show">Show</a>
-                    <a href="{{ route('products.edit', [$product]) }}" class="btn btn-edit">Edit</a>
-                    <form action="{{ route('products.destroy', [$product]) }}" method="post">
+                    <a href="{{ route('products.show', $product) }}" class="btn btn-show">Show</a>
+                    <a href="{{ route('products.edit', $product) }}" class="btn btn-edit">Edit</a>
+
+                    <form action="{{ route('products.destroy', $product) }}" method="post">
                         @csrf
                         @method('DELETE')
                         <input type="submit" value="Delete" class="btn btn-delete">
@@ -20,5 +36,4 @@
             </li>
         @endforeach
     </ul>
-
 </x-layout>
