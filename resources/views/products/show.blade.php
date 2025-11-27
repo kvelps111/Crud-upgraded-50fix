a25f1b8179fb1b27ed50f3054bfb5d4d566e57cc
user <test@gmail.com>
2025-11-27 10:45:44 +0200

    made model for tag

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -16,6 +16,7 @@
     <h1>{{ $product->name }}</h1>
     <h4>Quantity: {{ $product->quantity }}</h4>
     <p>{{ $product->description }}</p>
+    
 
     <a href="{{ route('products.edit', $product) }}">Edit</a>
     <form action="{{ route('products.destroy', $product) }}" method="post">
