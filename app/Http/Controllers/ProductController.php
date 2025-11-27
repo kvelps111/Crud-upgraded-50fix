9f3905aa01d5a455a5c26f1da7d428ad91a08b5f
Your Name <you@example.com>
2025-11-27 10:50:58 +0200

    RR

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,16 +7,19 @@ use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $products = Product::all();
         return view('products.index', compact('products'));
     }
 
-    public function create() {
+    public function create()
+    {
         return view('products.create');
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $validated = $request->validate([
             'name' => 'required|unique:products|max:255',
             'quantity' => 'required|integer',
@@ -26,27 +29,30 @@ class ProductController extends Controller
         $product = Product::create($validated);
 
         return redirect()
-                ->route('products.show', [$product]) // vai ['product' => $product]
-                ->with('message', "Product created successfully");
+            ->route('products.show', [$product]) // vai ['product' => $product]
+            ->with('message', "Product created successfully");
     }
 
-    public function show(Product $product, $id) {
-        $product=Product::find($id);
+    public function show(Product $product)
+    {
         return view('products.show', compact('product'));
     }
 
-    public function destroy(Product $product) {
+    public function destroy(Product $product)
+    {
         $product->delete();
         return redirect()
-                ->route('products.index')
-                ->with('message', "Product deleted successfully");
+            ->route('products.index')
+            ->with('message', "Product deleted successfully");
     }
 
-    public function edit(Product $product) {
+    public function edit(Product $product)
+    {
         return view('products.edit', compact('product'));
     }
 
-    public function update(Request $request, Product $product) {
+    public function update(Request $request, Product $product)
+    {
         $validated = $request->validate([
             'name' => 'required|unique:products|max:255',
             'quantity' => 'required|integer',
@@ -55,23 +61,24 @@ class ProductController extends Controller
 
         $product->update($validated);
         return redirect()
-                ->route('products.show', [$product])
-                ->with('message', "Product updated successfully");
+            ->route('products.show', [$product])
+            ->with('message', "Product updated successfully");
     }
 
-    public function decreaseQuantity(Request $request, Product $product) {
+    public function decreaseQuantity(Request $request, Product $product)
+    {
         $request->validate([
             'amount' => 'required|integer|min:1',
         ]);
 
         if ($product->decreaseQuantity($request->amount)) {
             return redirect()
-                    ->route('products.show', [$product])
-                    ->with('message', "Product quantity decreased successfully");
+                ->route('products.show', [$product])
+                ->with('message', "Product quantity decreased successfully");
         } else {
             return redirect()
-                    ->route('products.show', [$product])
-                    ->withErrors(['amount' => 'Not enough stock to decrease quantity.']);
+                ->route('products.show', [$product])
+                ->withErrors(['amount' => 'Not enough stock to decrease quantity.']);
         }
     }
 }
